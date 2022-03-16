76605f92fe7aa275ea84e0831e0a7f622c0ec0b2
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-03-17 00:21:28 +0530

    added headers

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ module.exports = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     REACT_APP_API_URL: process.env.REACT_APP_API_URL,
   },
+  async headers() {
+    return [
+      {
+        source: "/.*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   "presets": [ "next/babel" ],
   async rewrites() {
     return [
