be2b157001ed2db9e543698edb0c550a6cc444ce
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-03-22 00:52:22 +0530

    tailwind ssr support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,9 @@ const theme = createTheme( {
     },
   },
 } )
-
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 function MyApp ( { Component, pageProps }: AppProps ) {
   return (
     <UserProvider>
