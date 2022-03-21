ef26194e416b7e8f11d38487b673523a76e4622a
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-03-22 00:42:52 +0530

    cors

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  },
-  "presets": [ "next/babel" ],
-  async rewrites() {
-    return [
-      {
-        source: '/:prefix*/server/:path*',
-        destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/:path*' // Proxy to Backend
-      }
-    ]
-  }
+  // env: {
+  //   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  //   REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  //       ]
+  //     }
+  //   ]
+  // },
+  // "presets": [ "next/babel" ],
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:prefix*/server/:path*',
+  //       destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/:path*' // Proxy to Backend
+  //     }
+  //   ]
+  // }
 };
