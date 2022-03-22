e4e937d87aaf70fb1ffe6e62d9a3175be9b90be3
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-03-23 01:33:06 +0530

    removed swc-minify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  swcMinify: true,
+  // swcMinify: true,
   reactStrictMode: true,
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
