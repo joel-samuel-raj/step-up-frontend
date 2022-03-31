398c5a09014c19204ed9d8b163dde8ef7274d8ba
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-04-01 05:29:53 +0530

    done

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
         source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://stepup-quizapp.vercel.app/" },
+          { key: "Access-Control-Allow-Origin", value: "https://gct-quizapp.vercel.app/" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
